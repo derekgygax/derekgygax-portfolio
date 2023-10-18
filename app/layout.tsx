336d9e00918d0382293bf8f26e7d5a52cb0432b6
derekgygax <derekgygax@gmail.com>
2023-10-18 19:50:15 +0700

    sass configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,30 @@
 import type { Metadata } from 'next'
 
 // styles
-import './globals.css'
+import './globals.scss'
 
+
+// TODO put this data somewhere!!!
+// TODO put this data somewhere!!!
+// TODO put this data somewhere!!!
+// TODO put this data somewhere!!!
 export const metadata: Metadata = {
   title: 'Derek Gygax',
   description: 'Portfolio for Derek Gygax',
-
-
+  // TODO clean this shit up!
+  openGraph: {
+    type: 'website',
+    title: 'Derek Gygax Portfolio',
+    siteName: 'Derek Gygax Portfolio',
+    description: 'Portfolio for Derek Gygax',
+    // TODO add this
+    // url: 'https://57west.us',
+  },
+  // TODO IDK ABOUT THESE
+  viewport: 'width=device-width, initial-scale=1.0',
+  twitter: {
+    card: 'summary_large_image',
+  }
 }
 
 export default function RootLayout({
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const path = require('path')
+
+const nextConfig = {
+  // Add configuration to use sass for css
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
