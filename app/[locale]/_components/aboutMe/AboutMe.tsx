6871a6517bd9375d181caa1b36e8063821daec15
derekgygax <derekgygax@gmail.com>
2023-10-23 16:32:19 +0700

    aboutme kinda up

--- app/[locale]/_components/aboutMe/AboutMe.tsx
+++ app/[locale]/_components/aboutMe/AboutMe.tsx
@@ -1,12 +1,31 @@
+import { useTranslations } from "next-intl";
+
+// layouts
 import { Section } from "@/app/[locale]/_layouts/section/Section";
 
 // import data
-import aboutSection from '@/app/data/aboutMe.json';
+import aboutMe from '@/app/data/aboutMe.json';
+
+// styles
+import styles from './AboutMe.module.scss';
 
 export const AboutMe: React.FC = () => {
+
+  const t = useTranslations('AboutMe');
   return (
-    <Section id={aboutSection.id}>
-      <div>About Me</div>
+    <Section id={aboutMe.id}>
+      <h1>About Me</h1>
+      <div className={styles.aboutContent}>
+        <div className={styles.content}>
+          <p>{t("details")}</p>
+        </div>
+        <div className={styles.content}>
+          <img
+            src={aboutMe.image}
+            height={500}
+          />
+        </div>
+      </div>
     </Section>
   )
 }
