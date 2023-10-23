765a8dc2d49197d2ee972cb84326440dd4620a95
derekgygax <derekgygax@gmail.com>
2023-10-23 15:19:36 +0700

    added some footer

--- app/[locale]/_components/footer/Footer.tsx
+++ app/[locale]/_components/footer/Footer.tsx
@@ -1,10 +1,20 @@
-import { Container } from "@/app/[locale]/_layouts/container/Container"
+import { useTranslations } from "next-intl";
+
+// layouts
+import { Container } from "@/app/[locale]/_layouts/container/Container";
+
+// styles
+import styles from './Footer.module.scss';
 
 export const Footer: React.FC = () => {
+
+  const t = useTranslations('Footer');
   return (
-    <div>
+    <div className={styles.main}>
       <Container>
-        Footer
+        <div className={styles.copyright}>
+          <span>{`${t('copyright')}. ${t('buildDescription')}`}</span>
+        </div>
       </Container>
     </div>
   )
