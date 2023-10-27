ab38703e333794ba5727f57479c37bc45329587f
derekgygax <derekgygax@gmail.com>
2023-10-27 14:20:36 +0700

    text margin size change for phone

--- app/[locale]/_components/aboutMe/AboutMe.tsx
+++ app/[locale]/_components/aboutMe/AboutMe.tsx
@@ -17,19 +17,19 @@ export const AboutMe: React.FC = () => {
   return (
     <section id={aboutMe.id}>
       <div className="section">
-        <div className="pb-8">
+        <div className="pb-4 md:pb-8">
           <h1 className="m-0 text-5xl font-bold pb-2">
             {t('name')}
           </h1>
-          <h2 className="m-0 mt-1 font-semibold text-2xl">
+          <h2 className="m-0 mt-1 font-medium text-2xl">
             {t('jobTitles')}
           </h2>
         </div>
         <div className="flex flex-row">
           <div className="w-full md:w-2/3 flex flex-col">
-            <div className="text-xl text-secondary-text">
+            <div className="md:text-xl text-secondary-text">
               <div className="w:full md:w-11/12">
-                <p>{t("bio.work")}</p>
+                <p className="pb-2">{t("bio.work")}</p>
                 <p>
                   {t("bio.personal.general")}
                   <span className="hidden md:inline ml-1">
