9565f7e8a5da9fc03c871fd9602188ecff2dc9ff
derekgygax <derekgygax@gmail.com>
2024-01-08 21:28:14 +0700

    Technology model altText to alt

--- models/Technology.ts
+++ models/Technology.ts
@@ -1,18 +1,18 @@
 export class Technology {
   public name: string;
   public tooltip: string | null;
-  public altText: string | null;
+  public alt: string | null;
   constructor({
     name,
     tooltip,
-    altText
+    alt
   }: {
     name: string;
     tooltip?: string;
-    altText?: string;
+    alt?: string;
   }) {
     this.name = name;
     this.tooltip = tooltip || null;
-    this.altText = altText || null
+    this.alt = alt || null
   }
 }
