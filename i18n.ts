a2969d7718f5e33effdb1f6b51df8f73f50c8110
derekgygax <derekgygax@gmail.com>
2024-01-30 13:22:57 +0800

    temp "fix" to db loading translations

--- i18n.ts
+++ i18n.ts
@@ -8,31 +8,28 @@ import { getContactMeButtonTranslation } from "./lib/db/contactMeButton";
 import { getRootLayoutMetadata } from "./lib/db/rootLayout";
 import { getGeneralTranslations } from "./lib/db/general";
 
-const locales = LOCALES;
-
+// TODO You put the any there but you don't want that at all!!!
+// TODO You put the any there but you don't want that at all!!!
+// TODO You put the any there but you don't want that at all!!!
+// TODO You put the any there but you don't want that at all!!!
 export default getRequestConfig(async ({ locale }) => {
 
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!LOCALES.includes(locale as any)) notFound();
 
   return {
     messages: {
-      AboutMe: await Portfolio.getUserTranslations(),
-      ContactMeButton: await getContactMeButtonTranslation(),
+      AboutMe: await Portfolio.getUserTranslations() as any,
+      ContactMeButton: await getContactMeButtonTranslation() as any,
       Metadata: {
-        RootLayout: await getRootLayoutMetadata(),
+        RootLayout: await getRootLayoutMetadata() as any,
         ...(await Portfolio.getProjectsMetadata())
       },
-      General: await getGeneralTranslations(),
-      Sections: await Portfolio.getSectionTranslations(),
-      Icons: await getIconTranlastions(),
-
-      // TODO THE ONE BELOW is only here to keep the typescript from complaining
-      // you are figuring out why!!!
-
-      ...(await import(`./messages/${locale}/resume.json`)).default,
-      Project: await Portfolio.getProjectTranslations(),
-      Footer: await Portfolio.getFooterTranslations()
+      General: await getGeneralTranslations() as any,
+      Sections: await Portfolio.getSectionTranslations() as any,
+      Icons: await getIconTranlastions() as any,
+      Project: await Portfolio.getProjectTranslations() as any,
+      Footer: await Portfolio.getFooterTranslations() as any
     }
   }
 });
